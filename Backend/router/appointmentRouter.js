b59e0f3c316b9568656f1ef53c54a3110e58c88a
unknown <sp9257@srmist.edu.in>
2026-04-20 20:04:13 +0530

    fixed my appointments auth middleware

--- Backend/router/appointmentRouter.js
+++ Backend/router/appointmentRouter.js
@@ -11,12 +11,13 @@ import {
   isPatientAuthenticated,
 } from "../middlewares/auth.js";
 
+
 const router = express.Router();
 
 router.post("/post", isPatientAuthenticated, postAppointment);
 router.get("/getall", isAdminAuthenticated, getAllAppointments);
 router.put("/update/:id", isAdminAuthenticated, updateAppointmentStatus);
 router.delete("/delete/:id", isAdminAuthenticated, deleteAppointment);
-router.get("/my", isAuthenticated, getMyAppointments);
+router.get("/my", isPatientAuthenticated, getMyAppointments);
 
 export default router;
